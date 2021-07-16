94f2d82f0c11439236dbf0a10274dd5b193c5979
Quan215 <70443949+Quan215@users.noreply.github.com>
2021-07-16 13:20:42 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 with open("text.txt", "r", encoding="utf8") as markdown:
     markdownContent = markdown.read()
+    print(markdownContent)
 with open("text.txt", "w", encoding="utf-8") as markdown:
     markdown.write(f"{markdownContent}1{markdownContent}")
+    print(f"{markdownContent}1{markdownContent}")
